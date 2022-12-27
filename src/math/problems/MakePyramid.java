cf1cd1e3327be718ddd5c0244d285e5257367489
ismailleghzali <leghzali7@gmail.com>
2022-12-26 20:04:25 -0600

    first push

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,7 +1,25 @@
 package math.problems;
 
+import sun.awt.geom.AreaOp;
+
 public class MakePyramid {
     public static void main(String[] args) {
+        int rows=7; s=0;
+        for(int i=1; i<=rows; ++i, s=0 ){
+            for (int spa = 1, space <= rows-i; ++spa)
+
+                System.out.println(" ");
+
+while (s != 2 * i - 1) {
+    System.out.println("* ");
+    ++s;
+            }
+            System.out.println();
+        }
+
+        }
+    }
+
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
@@ -15,5 +33,3 @@ public class MakePyramid {
         */
 
 
-        }
-  }
